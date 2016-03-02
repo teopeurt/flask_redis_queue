822563d0b67e3b45892e01c1dca4a9f519d4d2e7
don Onwunum <don@pigstycoders.com>
2016-03-02 06:40:38 +0100

    initial commit

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class ContactForm(Form):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email")
+    subject = StringField("Subject")
+    message = TextAreaField("Message")
+    submit = SubmitField("Send")
---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+
+app = Flask(__name__)
+app.secret_key = 'development key'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All Field Required')
+            return render_template('contact.html', form=form)
+        else:
+            return 'Form Postado'
+
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
